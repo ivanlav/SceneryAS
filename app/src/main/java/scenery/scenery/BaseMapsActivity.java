24471327d724b57894130c6fc7f5e0733d4b9247
Ivan <lavrentyev.ivan@gmail.com>
2017-08-18 12:38:18 -0400

    (Refactoring) Set to Inherit from BaseMapsActivity

---
+++ app/src/main/java/scenery/scenery/BaseMapsActivity.java
@@ -0,0 +1,12 @@
+package scenery.scenery;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+/**
+ * Created by Ivan on 8/18/2017.
+ */
+
+public class BaseMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+}
